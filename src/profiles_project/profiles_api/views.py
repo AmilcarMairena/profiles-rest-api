a59e8bf2d203961a976df61f9944f4ce6b2046ab
AmilcarMairena <48960686+AmilcarMairena@users.noreply.github.com>
2020-11-26 21:03:19 -0600

    Added model for storing profile feed items, created migrations and added to django admin

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -117,7 +117,7 @@ class UserProfileUsers(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permissions_classes = (permissions.UpdateOwnProfile,)
+    permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
 
